61828c4db1b631987ffac806c2de6b79eda6307f
sen zhan <sen.zhan@outlook.com>
2026-01-07 21:00:54 +0100

    Update test cases

--- scripts/solve_case.py
+++ scripts/solve_case.py
@@ -2,7 +2,7 @@ from routing_optimization.main import main
 from routing_optimization.utils import visualize_route
 from pathlib import Path
 
-data_file = Path(__file__).resolve().parent.parent / "data" / "simple_12_events.json"
+data_file = Path(__file__).resolve().parent.parent / "data" / "case2.json"
 result = main(data_file)
 
 print(result)
